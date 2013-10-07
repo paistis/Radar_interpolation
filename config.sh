05ae680c2030f0302fe0f69d6870fc96fbeec3fb
Nordling Kalle G <nordlikg@lx1-flatm-brus.atm.helsinki.fi>
2013-10-07 13:40:15 +0300

    config file update

---
+++ config.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+#example
+file1="/home/nordlikg/fmi/HDF5/20100808/VAN/201008081800_VAN.PPI1_A.raw.h5_with_rayHdrs_F.h5"
+file2="/home/nordlikg/fmi/HDF5/20100808/VAN/201008081815_VAN.PPI1_A.raw.h5_with_rayHdrs_F.h5"
+timesteps=16
+var="Z2"
+sweep=2
+site="VAN"
+images="images/"
+morp_output="morp/" #output forder for interpolated images
