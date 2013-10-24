139808d8aa73613fe4c4efed932a94c45aa7d801
Nordling Kalle G <nordlikg@lx1-flatm-brus.atm.helsinki.fi>
2013-10-24 13:44:57 +0300

    adding py-art support and python version of interpolation

---
+++ geotiff2png.py
@@ -0,0 +1,124 @@
+#!/usr/bin/python
+import sys, os, time
+from scipy import ndimage
+from scipy import misc
+import scipy.io
+from matplotlib.image import AxesImage
+from pylab import *
+import gdal,cmath
+import numpy as np
+import matplotlib.pyplot as plt
+import Image
+
+
+# MAIN
+
+def geotiff2png(ifile,var,ofile,bit8=False):
+	print "ifile: " + ifile
+	print "ofile: " + ofile
+	if os.path.exists(ifile):
+	    ds = gdal.Open(ifile)
+	    ilbm = ds.GetRasterBand(1).ReadAsArray()
+	    h,w = ilbm.shape
+	    size = int(2**(ceil(log(max(w,h))/log(2))))
+	    print "size: " + str(size)
+	    if (var == 'reflectivity_horizontal'):
+		
+		if(bit8 == True):
+			#8bit data
+			ilbm[ilbm >= 95.5] = nan
+			ilbm[ilbm < -31.5] = nan
+			ilbm = np.round((2.0*ilbm+64.0))
+			ilbm = ilbm.astype('int8')
+			img = scipy.misc.toimage(ilbm)
+			img = img.resize((size,size))
+			img.save(ofile)
+			return 0
+
+		ilbm[ilbm >= 327.66] = nan
+	   	ilbm[ilbm <= -327.67] = nan
+		ilbm = np.round((100.0*ilbm+32768.0))
+	    #KDP2 from sigmet.py
+	    if (var == "diff_phase"):
+		ilbm = np.round(100.0*ilbm+32768)
+		ilbm[ilbm <= 0] = nan
+		ilbm[ilbm >= 65535] = nan
+	    #RHOHV2 from sigmet.py
+	    if (var == "copol_coeff"):
+		ilbm = np.round((ilbm*65533)+1)
+		ilbm[ilbm <= 0] = nan
+		ilbm[ilbm >= 65535] = nan
+
+	    if (var == "mean_doppler_velocity"):
+		ilbm = np.round(100.0*ilbm+32768)
+		ilbm[ilbm <= 0] = nan
+		ilbm[ilbm >= 65535] = nan
+
+	    ilbm = ilbm.astype('int32')
+	    img = scipy.misc.toimage(ilbm,high=np.max(ilbm),low=np.min(ilbm),mode='I')
+	    img = img.resize((size,size))
+	    img.save(ofile)
+	else:
+	    print "File " + ifile+ " does not exist"
+
+def png2geotiff(ifile,var,ofile,grid):
+
+	grid_shape = grid.fields[var]['data'].shape
+	nz, ny, nx = grid_shape
+	rcells = ny*0.5
+	dist = max(grid.axes['x_disp']['data'])
+	rangestep = grid.axes['x_disp']['data'][1] - grid.axes['x_disp']['data'][2]
+    	lat = grid.axes['lat']['data'][0]
+	lon = grid.axes['lon']['data'][0]
+
+	grid_shape = grid.fields[var]['data'].shape
+	nz_d, ny_d, nx_d = grid_shape
+
+    	Img = misc.imread(ifile)
+    	arr = np.asarray(Img)
+	lbm2=Img
+
+	if (var =='reflectivity_horizontal'):
+		#lbm2[lbm2 >= 65534] = nan
+		#lbm2[lbm2 <= 1] = nan
+		lbm2 = (lbm2-32768.0)/100.0
+		lbm2[lbm2 >= 327.66] = nan
+   		lbm2[lbm2 <= -325.00] = nan
+
+	if (var =='diff_phase'):
+		#lbm2[lbm2 >= 65534] = nan
+		#lbm2[lbm2 <= 1] = nan
+		lbm2 = (lbm2-32768.0)/100.0
+		lbm2[lbm2 >= 327.66] = nan
+   		lbm2[lbm2 <= -325.00] = nan
+
+	if (var =='mean_doppler_velocity'):
+		#lbm2[lbm2 >= 65534] = nan
+		#lbm2[lbm2 <= 1] = nan
+		lbm2 = (lbm2-32768.0)/100.0
+		lbm2[lbm2 >= 327.66] = nan
+   		lbm2[lbm2 <= -325.00] = nan
+
+	if (var =='copol_coeff'):
+		#lbm2[lbm2 >= 65534] = nan
+		#lbm2[lbm2 <= 1] = nan
+		lbm2 = (lbm2-32768.0)/100.0
+		lbm2[lbm2 >= 327.66] = nan
+   		lbm2[lbm2 <= -325.00] = nan
+
+	nx,ny = arr.shape
+
+	del_x = ((nx-nx_d)/2.0)
+	del_y = ((ny-ny_d)/2.0)
+	lbm3 = lbm2[del_x:nx_d+del_x,del_y:ny_d+del_y]
+
+	out_driver = gdal.GetDriverByName("GTiff")
+	dst_options = ['COMPRESS=LZW','ALPHA=YES']
+	dst_ds = out_driver.Create(ofile, int(2*rcells), int(2*rcells), 1, gdal.GDT_Float32, dst_options)
+	dst_ds.SetGeoTransform( [ dist, -rangestep, 0, -dist, 0, rangestep ] )
+
+	iproj = 'PROJCS["unnamed",GEOGCS["WGS 84",DATUM["unknown",SPHEROID["WGS84",6378137,298.257223563]],PRIMEM["Greenwich",0],UNIT["degree",0.0174532925199433]],PROJECTION["Azimuthal_Equidistant"],PARAMETER["latitude_of_center",'+str(lat)+'],PARAMETER["longitude_of_center",'+str(lon)+'],PARAMETER["false_easting",0],PARAMETER["false_northing",0],UNIT["metre",1,AUTHORITY["EPSG","9001"]]]'
+	dst_ds.SetProjection(iproj)
+
+    	dst_ds.GetRasterBand(1).WriteArray( lbm3)	    
+
---
+++ interpolation.py
@@ -0,0 +1,108 @@
+import sys, os, time
+from scipy import ndimage
+from scipy import misc
+import scipy.io
+from matplotlib.image import AxesImage
+from pylab import *
+import gdal,cmath
+import numpy as np
+import pyart
+import geotiff2png
+import subprocess
+import glob
+
+# Variables
+RADAR_FILE1 = '201008081800_VAN.PPI1_A.raw'
+RADAR_FILE2 = '201008081805_VAN.PPI2_A.raw'
+movement_variable = 'reflectivity_horizontal'
+interpolated_variables = ['reflectivity_horizontal']
+timesteps = 10
+images = "images/"
+morph = "morp/"
+filename="test"
+path = os.getcwd()
+
+#Read radar data 
+radar1 = pyart.io.read_rsl(RADAR_FILE1)
+radar2 = pyart.io.read_rsl(RADAR_FILE2)
+
+dist = radar1.range['data'][-1]
+rcells = radar1.ngates
+# mask out last 10 gates of each ray, this removed the "ring" around th radar.
+radar1.fields['reflectivity_horizontal']['data'][:, -10:] = np.ma.masked
+radar2.fields['reflectivity_horizontal']['data'][:, -10:] = np.ma.masked
+
+
+# perform Cartesian mapping, limit to the reflectivity field.
+
+grid1 = pyart.map.grid_from_radars(
+    (radar1,),
+    grid_shape=(rcells*2, rcells*2, 5),
+    grid_limits=((dist, -dist), (dist, -dist),
+                 (10, 10)),
+    fields=interpolated_variables)
+grid1.write(filename+"_grid1.nfc")
+
+grid2 = pyart.map.grid_from_radars(
+    (radar2,),
+    grid_shape=(rcells*2, rcells*2, 5),
+    grid_limits=((dist, -dist), (dist, -dist),
+                 (10, 10)),
+    fields=interpolated_variables)
+grid1.write(filename+"_grid2.nfc")
+
+grid1.write(images+RADAR_FILE1+"tiff",'GTiff')
+grid2.write(images+RADAR_FILE2+"tiff",'GTiff')
+
+
+#at this point we have start and end images for the interpolation
+#step 1. make grayscale images: 8-bit for calculating motion field, 16-bit for interpolation (output formats are png) and size is next power of 2
+image1 = images+RADAR_FILE1  +"_"+movement_variable+".tiff"
+image2 = images+RADAR_FILE2 +"_"+movement_variable+".tiff"
+image1_png = images+RADAR_FILE1 +"_"+movement_variable+"_8bit"+".png"
+image2_png = images+RADAR_FILE2 +"_"+movement_variable+"_8bit"+".png"
+
+geotiff2png.geotiff2png(image1,movement_variable,image1_png,True)
+geotiff2png.geotiff2png(image2,movement_variable,image2_png,True)
+
+#convert interpolated values to 16bit png's
+
+for i in interpolated_variables:
+	geotiff2png.geotiff2png(images+RADAR_FILE1 +"_"+ i+".tiff",i,images+RADAR_FILE1 +i+".png",False)
+	geotiff2png.geotiff2png(images+RADAR_FILE2 +"_"+ i+".tiff",i,images+RADAR_FILE2 +i+".png",False)
+
+
+
+#step 2. calculate motion vectors from movement_variable
+
+args = path+"/optflow_8bit/build/bin/morph --image1 "+image1_png+" --image2 "+image2_png+" --numtimesteps "+str(timesteps)+" --algorithm proesmans --outprefix "+filename+"_motion"
+os.system(args)
+
+#step 2. interploate all interpolation variables using 16-bit optical flow algorithm
+
+path = os.getcwd()
+for i in interpolated_variables:
+	image1 = images+RADAR_FILE1 +i+".png"
+	image2 = images+RADAR_FILE2 +i+".png"
+	vec1 = filename+"_motion-motion1.pdvm"
+	vec2 = filename+"_motion-motion2.pdvm"
+
+	args=path+"/optflow/build/bin/morph3 --image1 " +image1+" --image2 "+image2+" --numtimesteps "+str(timesteps)+ " --algorithm proesmans --outprefix " + filename+"_"+i + " --vec1 "+vec1+" --vec2 "+ vec2
+	os.system(args)
+
+#step 3 make geotiff's from interpolated images
+for i in interpolated_variables:
+	ifiles = glob.glob('*reflectivity_horizonta*.png')
+	for ifile in ifiles:
+		ofile, ext = os.path.splitext(ifile)
+		ofile = path+"/"+morph+ofile + ".tiff"
+		geotiff2png.png2geotiff(ifile,i,ofile,grid1)
+		os.remove(ifile)
+
+
+
+		
+
+
+
+
