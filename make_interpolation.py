f08879e8a865e9f5bee9c683f4fc7e16216dfc42
Nordling Kalle G <nordlikg@dx0-flatm-dda.xrayphys.helsinki.fi>
2013-11-19 15:54:23 +0200

    login working now

--- make_interpolation.py
+++ make_interpolation.py
@@ -22,14 +22,16 @@ except:
 	print "Site folder already exist.. not creating it"
 
 for step in time_intervals:
-	for i in range(0,step):
+	for i in range(0,60/step):
 
 		timesteps=step+1
-		start=start_time+i
+		start=start_time+i*step
 		end=start+step
-		RADAR_FILE1=data_path+glob.glob(str(date)+str(start)+"_VAN.PPI*_A.raw")[0]
-		RADAR_FILE2=data_path+glob.glob(str(date)+str(end)+"_VAN.PPI*_A.raw")[0]
-
+		if end >= start_time+60:
+			end=end_time
+		RADAR_FILE1=glob.glob(data_path+"/"+str(date)+str(start)+"_VAN.PPI*_A.raw")[0]
+		RADAR_FILE2=glob.glob(data_path+"/"+str(date)+str(end)+"_VAN.PPI*_A.raw")[0]
+		
 		name=str(start)+"-"+str(end)
 		try:
 			os.mkdir(site+"/"+str(date)+"/"+name)
@@ -48,16 +50,17 @@ for step in time_intervals:
 		images=site+"/"+str(date)+"/"+name+"/image/"
 		morp=site+"/"+str(date)+"/"+name+"/morph/"
 		filename=str(date)+"_"+site+"_"+name	
-	
+		
 		interpolation.interpolate(RADAR_FILE1,RADAR_FILE2,timesteps,images,morp,filename)
-
+		
 		#write log
-		fo=open((site+"/"+str(date)+"/"+name+"/"+name+".log"),"w+")
+		fo=open((site+"/"+str(date)+"/"+name+"/"+name+".log"),"w")
 		fo.write("site: "+site+"\n")
 		fo.write("timestep: "+str(timesteps)+"\n")
 		fo.write("name: "+name+"\n")
-		fo.write("RADAR_FILE1: "+RADAR_FILE1)
-		fo.write("RADAR_FILE2: "+RADAR_FILE2)
+		fo.write("RADAR_FILE1: "+RADAR_FILE1+"\n")
+		fo.write("RADAR_FILE2: "+RADAR_FILE2+"\n")
 		fo.write("filename: "+filename)
-
+		fo.close()
+		
 		
